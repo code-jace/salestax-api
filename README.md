f7423c87efb343b0ca37edd64711328592d45f36
Richard Rodger <richard@ricebridge.com>
2015-05-11 14:00:29 +0100

    docker

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # salestax-api
 salestax-api
+
+
+# v2 docker
+
+```sh
+docker build -t salestax-api-v1 .
+docker run -p 51000:51000 -e WEB_HOST=0.0.0.0 -d salestax-api-v1 node app.js
+```
--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 require('express')()
 
   .get('/ping',function( req, res ){
-    res.send( 'v2: '+new Date().toISOString() )
+    res.send( 'v1: '+new Date().toISOString() )
   })
 
-  .listen( parseInt(process.argv[2],10) || 51000 )
+  .listen( parseInt(process.argv[2],10) || 51000, 
+           process.env.WEB_HOST || 'localhost' )
